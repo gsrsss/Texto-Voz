a218344f07c579f4f98eab5ee15555031b07c7cc
gsrsss <gsrsotos@gmail.com>
2025-09-05 12:26:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from gtts import gTTS
 from PIL import Image
 import base64
 
-st.title("Resumenes para todos")
+st.title("Resúmenes en voz alta (˶ᵔ ᵕ ᵔ˶)")
 image = Image.open('gato_raton.png')
 st.image(image, width=350)
 with st.sidebar:
-    st.subheader("Quieres ver una serie nueva pero simplemente leer el resumen no es suficiente? Escribe cualquier texto y escuchalo aquí!")
+    st.subheader("Quieres ver una serie nueva pero simplemente leer el resumen no es suficiente? Escribe cualquier texto y escuchalo aquí! ⋆౨ৎ˚⟡˖ ࣪")
 
 
 try:
